9213712b35e9c9acec8615bccb39b3ad73673330
guido9j <james_guido@homedepot.com>
2022-09-02 00:35:32 -0400

    Logging test (#1)
    * cmd/arcade/arcade.go: Set Flags for logging
    
    * internal/rancher/client.go: Add logging when getting token fails
    
    * internal/rancher/client_test.go: Added 2 test cases to cover new code paths for logging
    
    * build.sh: Add script to build arcade (same process can now be used manually and workflow files)
    
    * get-dependencies.sh: Add script to get build dependencies (same process can now be used manually and workflow files)
    
    * docker/build.sh → docker/docker-build.sh: Rename to avoid confusion with build.sh and call get-dependencies.sh and build.sh instead of duplicating logic
    
    * .github/workflows/go.yml: Updated to call get-dependencies.sh and build.sh instead of duplicating logic
    
    * .github/workflows/release.yml: New workflow with version parameter to build docker image

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -4,8 +4,9 @@ set -o errexit
 
 # Builds the base image including the solver dependencies
 build_and_publish_image(){
-    set -x
-    GOOS=linux GOARCH=amd64 go build --ldflags '-extldflags "-static"' cmd/arcade/arcade.go
+    export PATH=.:$PATH
+    get-dependencies.sh >/dev/null
+    GOOS=linux GOARCH=amd64 build.sh
     GCR_TAG="oshomedepot/arcade:${TAG_VERSION}"
     docker build . -f docker/Dockerfile -t ${GCR_TAG}
     echo "Image ${GCR_TAG} built..."
